e56669e74d3442bdce8adc640a9388f70f7e66ca
wujiaan2022 <emmawu2004@gmail.com>
2023-02-05 21:29:01 +0000

    set the board class and add methods of print adn guess

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+class Board:
+    """
+    main board setting of size, player's name, numbers of ships, 
+    board type(player's board or computer's). Has methods for 
+    adding ships and guesses and print the board
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+    
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "x"
+
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def add_ship(self, x, x, type = "computer"):
+        if len(self.ship) >= self.num_ships:
+            print("Error: you can not add any more ships!")
+        else:
+            self.ship.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+
+def random_point(size):
+        """
+        return random integer between 0 and size
+        """
+        return randint(0, size-1)
+
+
