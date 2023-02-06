e89a61df073d3ebba41ccaa19fd29425bb0915de
wujiaan2022 <emmawu2004@gmail.com>
2023-02-06 13:06:35 +0000

    set up valid coordinates function

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ class Board:
         else:
             return "Miss"
 
-    def add_ship(self, x, x, type = "computer"):
+    def add_ship(self, x, y, type = "computer"):
         if len(self.ship) >= self.num_ships:
             print("Error: you can not add any more ships!")
         else:
@@ -44,5 +44,58 @@ def random_point(size):
         return random integer between 0 and size
         """
         return randint(0, size-1)
+
+def valid_coordinates(x, y, board):
+    try:
+        x = int(input("Please enter a number: "))
+        y = int(input("Please enter a number: "))
+        print(f"row = {x} column = {y}")
+    except Exception:
+        if x > 4 or y >4:
+            print("Please enter a number between 0 and 4.")
+    except ValueError:
+        if float(x) == x or float(y) == y:
+            print("Both values have to be integers.")
+    else:
+        print('Another error has occurred')
+
+def populate_board(board): 
+    x = random_point(size)
+    y = random_point(size)
+
+
+def make_guess(board):
+
+
+def play_game(computer_board, player_board):
+
+
+def new_game():
+    """
+    Starts a new game. Sets the board size and numbers of ships, 
+    resets the scores and initialises the baords.
+    """
+    size = 5
+    num_ships = 4
+    scores["computer"] = 0
+    scores["player"] = 0
+    print("-" * 35)
+    print("Welcom to...")
+    print(f"Board size is {size}. Number of ships is {num_ships}")
+    print("Top left coner is row: 0, col: 0")
+    print("-" * 35)
+    play_name = input("Please enter your name: \n")
+    print("-" * 35)
+
+    computer_board = Board(size, num_ships, "computer", type = "computer")
+    player_board = Board(size, num_ships, play_nam, type = "player")
+
+    for _ in range(num_ships):
+        populate_board(player_board)
+        populate_board(computer_board)
+    
+
+
+
 
 
